0463f0b23fb2d86371e1168521e6b2427b01f9e2
Erick Scotta Turcatto <125623284+ErickScottaTurcatto@users.noreply.github.com>
2026-08-26 15:08:17 -0300

    Add header file for linked list implementation

---
+++ lista.h
@@ -0,0 +1,48 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+#include <stdbool.h>
+
+typedef struct lista *Lista;
+typedef int dado_t;
+
+// cria e inicializa uma lista vazia
+Lista l_cria();
+
+// libera a memória ocupada por uma lista
+void l_destroi(Lista l);
+
+// retorna true se a lista tiver cheia
+bool l_cheia(Lista l);
+//
+// retorna true se a lista tiver vazia
+bool l_vazia(Lista l);
+
+// insere o dado d no início da lista l
+void l_insere_inicio(Lista l, dado_t d);
+
+// insere o dado d no final da lista l
+void l_insere_fim(Lista l, dado_t d);
+
+// insere o dado d na lista l, de forma que ele fique na posição p
+void l_insere_pos(Lista l, dado_t d, int p);
+
+// retorna o dado no início da lista
+dado_t l_dado_inicio(Lista l);
+
+// retorna o dado no final da lista
+dado_t l_dado_fim(Lista l);
+
+// retorna o dado na posição pos da lista
+dado_t l_dado_pos(Lista l, int pos);
+
+// remove e retorna o dado no início da lista
+dado_t l_remove_inicio(Lista l);
+
+// remove e retorna o dado no final da lista
+dado_t l_remove_fim(Lista l);
+
+// remove e retorna o dado na posição pos da lista
+dado_t l_remove_pos(Lista l, int pos);
+
+#endif // LISTA_H
